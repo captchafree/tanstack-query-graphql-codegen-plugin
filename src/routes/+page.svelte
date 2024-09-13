254e3b233191e7f7e8ede0d504480016f1a0aced
Josh Beck <j.beck18@me.com>
2024-09-13 16:58:20 -0400

    reformat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    import { getUserById } from "../generated/functions";
-    import { createQuery } from '@tanstack/svelte-query'
-    import type { PageData } from './$types'
+  import { getUserById } from "../generated/functions";
+  import { createQuery } from '@tanstack/svelte-query'
+  import type { PageData } from './$types'
 
   export let data: PageData;
   
   const query = createQuery({
     ...getUserById({ id: '1' }),
   })
-
 </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- src/routes/page.ts
+++ src/routes/page.ts
@@ -1,10 +1,10 @@
 import { getUserById } from "../generated/functions"
 
 export async function load({ parent, fetch }) {
-    const { queryClient } = await parent()
-  
-    await queryClient.prefetchQuery({
-      ...getUserById({ id: '1' }, fetch)
-    })
-  }
+  const { queryClient } = await parent()
+
+  await queryClient.prefetchQuery({
+    ...getUserById({ id: '1' }, fetch)
+  })
+}
   
